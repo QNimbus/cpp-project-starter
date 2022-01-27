b9812cc186b4b7af5634b4e3275f2fb8c1820995
B. van Wetten <bas@vanwetten.com>
2022-01-27 16:32:34 +0100

    Changes
    - Added .editorconfig
    - Cleanup and housekeeping in CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 
 #include <fmt/format.h>
-#include <spdlog/spdlog.h>
-
-#ifndef CMAKEMACROSAMPLE
-#   define CMAKEMACROSAMPLE "NO SYSTEM NAME"
-#endif
-
-auto sum(int a, int b) {
-  return a + b;
-}
 
 int main() {
-  std::cout << "Hello CMake!\n";
+  std::vector<std::string> vec = { "This", "vector", "of", "strings" };
 
-  std::cout << CMAKEMACROSAMPLE << '\n';
+  for (auto s : vec) {
+    fmt::print("{} ", s);
+  }
 
-  std::cout << "Sum of 3 + 4 : " << sum(3, 4) << '\n';
+  fmt::print("was made possible by precompiled headers!");
 
   std::cin.get();
 
